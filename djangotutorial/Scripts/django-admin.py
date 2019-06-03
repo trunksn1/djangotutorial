81b64d5618d1003953a08c054c3f158bfc13d1c8
jacopo <trunksn1@gmail.com>
2019-06-03 21:49:38 +0200

    Primo commit di Djangogirls

---
+++ djangotutorial/Scripts/django-admin.py
@@ -0,0 +1,5 @@
+#!e:\clouding\dropbox\informatica\github\djangotutorial\djangotutorial\scripts\python.exe
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
